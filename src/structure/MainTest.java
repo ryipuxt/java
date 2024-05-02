5033e2ae83ef903c5513a0aa95a15d35c3dd7d3b
kdtKKH <gibbon0510@gmail.com>
2024-05-02 11:05:43 +0900

    asd

---
+++ src/structure/MainTest.java
@@ -0,0 +1,19 @@
+package structure;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		TencoIntArray tencoIntArray = new TencoIntArray();
+		
+		tencoIntArray.addElement(100);
+		tencoIntArray.addElement(200);
+		tencoIntArray.addElement(300);
+		tencoIntArray.addElement(400);
+		tencoIntArray.addElement(500);
+		
+		tencoIntArray.printAll();
+		
+	}
+
+}
